94a29e2b905e3e84420136d1d39c1009a254439b
Filipe M. Silva <filipemelodasilva@gmail.com>
2024-07-01 01:03:08 -0300

    chore: move services to a services folder

---
+++ app/src/app/services/pokemon.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+
+@Injectable()
+export class PokemonService {
+  private host: string = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      return of(result as T);
+    };
+  }
+
+  public getAllPokemons(page: number = 1) {
+    return this.http
+      .get(`${this.host}/pokemon?page=${page}`)
+      .pipe(catchError(this.handleError('getUser')));
+  }
+}
